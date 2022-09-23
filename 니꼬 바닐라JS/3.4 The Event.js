c2a738f0183b6f7c0423c9202c1f6f8577d2f1aa
Shin-googoo <111329350+Shin-googoo@users.noreply.github.com>
2022-09-24 01:16:23 +0900

    Add files via upload

---
+++ 니꼬 바닐라JS/3.4 The Event.js
@@ -0,0 +1,10 @@
+const h1 = document.querySelector("div.hello:first-child h1");
+
+console.dir(h1);
+
+function handleTitleClick(){
+  h1.style.color = "blue";
+}
+
+
+h1.addEventListener("click", handleTitleClick);
