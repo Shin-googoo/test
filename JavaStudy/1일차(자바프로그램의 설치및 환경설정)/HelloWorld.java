39a61e5467205584bdb8894988d565385588f3fb
Shin_Yong Goo <111329350+Shin-googoo@users.noreply.github.com>
2022-11-29 21:08:04 +0900

    Add files via upload

---
+++ JavaStudy/1일차(자바프로그램의 설치및 환경설정)/HelloWorld.java
@@ -0,0 +1,12 @@
+//comment
+/*
+   HelloWorld.java
+   source file
+*/
+
+ public class HelloWorld { //className start
+     	
+      public static void main(String args[]){
+          System.out.print("HelloWorld"); //rule
+       }
+} //end
