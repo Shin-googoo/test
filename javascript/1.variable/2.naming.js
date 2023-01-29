b2b4b123750b5c5b105dfa3c9762e915b3233140
Shin_Yong Goo <111329350+Shin-googoo@users.noreply.github.com>
2023-01-29 23:40:35 +0900

    Add files via upload

---
+++ javascript/1.variable/2.naming.js
@@ -0,0 +1,33 @@
+// 주석을 달면 됨
+/*
+* 변수 규칙!
+* 라틴문자(0-9, a-z, A-Z,), _
+* 대소문자를 구문함
+* 추천: camelCase (likeThis)
+* 한국어 ❌
+* 예약어 ❌
+* 숫자로 시작 ❌
+* 특수문자 ❌ (_,$ 두가지는 예외)
+* 이모지 ❌
+* 여러개의 변수를 1,2,3 구분 ❌ -> 최대한 의미있게, 구체적인 이름으로 작성!
+*/
+
+// 나쁜예제
+let number = 20;
+
+// 좋은예제
+let myAge = 20;
+
+// 나쁜예제
+let audio1;
+let audio2;
+
+// 좋은예제
+let backggroundAudio;
+let windAudio;
+
+//꿀팁
+let audioBackground;
+let audioWind;
+
+audioBackground;
---
+++ javascript/1.variable/6.empty.js
@@ -0,0 +1,15 @@
+// null, undefined
+
+let variable;
+console.log(variable);
+//변수에 값이 있는지 없는지 정해지지 않은 상태
+
+variable = null;
+console.log(variable);
+//변수는 비어있다!
+
+let activeItem; // 아직 활성화된 아이템이 있는지 없는지 모르는 상태!
+activeItem = null; // 활성화된 아이템이 없는 상태!
+
+console.log(typeof null);
+console.log(typeof undefined);
