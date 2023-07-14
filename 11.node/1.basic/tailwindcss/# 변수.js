c666377399915c8eed4e9fdab1a0780ed4cb6687
Shin_Yong Goo <111329350+Shin-googoo@users.noreply.github.com>
2023-07-14 23:41:51 +0900

    Add files via upload

---
+++ 11.node/1.basic/tailwindcss/# 변수.js
@@ -0,0 +1,67 @@
+# 변수
+a = 1
+b = 10.1
+c = '1'
+d = 'lee'
+e = 'hojun'
+f = true #javascript에서는 true입니다.
+g = [1, 2, 3, 4]
+h = (1, 2, 3, 4)
+i = {'one' : '하나', 'two' : '둘'}
+j = {1,2,3,3,3,3,3,4,4,4,}
+
+type(a)
+
+test = '11'
+print(int(test) + int(test))
+print(int(test) + int(test))
+print(int(test) + int(test))
+print(int(test) + int(test))
+print(int(test) + int(test))
+print(int(test) + int(test))
+
+
+
+set(['호준', '길동', '길동','길동','길동','호준'])
+{'길동', '호준'}
+
+set(1111111222222222333333)
+
+
+호준 = print
+호준 = (hello world)
+
+name = 'hojun'
+age = 10
+print('안녕하세요. 제이름은', name, '입니다. 제 나이는', age, '입니다.') # 예전문법
+print('안녕하세요. 제이름은 {} 입니다. 제 나이는 {} 입니다.') 
+.format(name, age)# 예전문법
+print(f'안녕하세요. 제이름은 {name} 입니다. 제 나이는 {age} 입니다.') # o
+
+
+# 산술연산
+x = 3
+y = 7
+print(x + y)
+print(x - y)
+print(x / y)    #float
+print(x / / y)  #int
+print(x * y)
+print(x % y)
+print(x ** y)
+
+
+#논리연산
+x = true  # 1
+y = false # 0
+
+# and : 곱
+# or : 합
+
+print(x and y)
+print(x or y)
+
+
+for i in Range(100)
+  if i % 3 == 0 and i % 5 == 0;
+    print(i)
