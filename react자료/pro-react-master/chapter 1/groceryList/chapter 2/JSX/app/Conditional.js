1294b2da211b99ccbe146135cc846f7b9d565216
Shin_Yong Goo <111329350+Shin-googoo@users.noreply.github.com>
2024-03-06 23:54:55 +0900

    Add files via upload

---
+++ react자료/pro-react-master/chapter 1/groceryList/chapter 2/JSX/app/Conditional.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+// Parent Component
+class Conditional extends Component {
+
+  render() {
+    let condition = true;
+    return (
+      <div>
+        {condition ?
+          <span>Hello JSX (The condition was true)</span>
+        : null}
+      </div>
+    );
+  }
+}
+
+export default Conditional
